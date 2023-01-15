1a7121e692663d1066811c695abd4a3181466599
LordShola <56320972+LordShola@users.noreply.github.com>
2023-01-15 23:35:57 +0100

    Own Data Hosting + Input Change
    got rid of external need (keeping it for backup maybe) + did some reworking for Input

--- connector.py
+++ connector.py
@@ -1,13 +1,13 @@
 def get_code_from_secret(location,password):
     file2.connect_to_secure(location,password)
 
-def get_input():
-    input=""
-    while help.get_input_stream()==True:
-        input+=help.get_input_from_keyboard()
-
-    return input
 
 
-perma_link=get_code_from_secret("https://pastebin.com/SyjRsXih",get_input())
+perma_link=get_code_from_secret("https://localhost:8080/data",help.get_complete_input_from_keyboard())
+#debug testing
+file2.debug("Test1")
+help.cool_stuff()
+help.more_stuff(10)
+file2.debug("test:"+str(file2.sha256(help.get_complete_input_from_keyboard())))
 help.create_super_mega_ultra_cool_thing(perma_link)
+file2.debug("part 2 worked!")
