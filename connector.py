c6030f573c4b81372cafc01d68c4bc82d17c2f02
LordShola <56320972+LordShola@users.noreply.github.com>
2023-01-15 23:05:14 +0100

    Finished Tests
    All things work, though you need new pastebins everytime...

--- connector.py
+++ connector.py
@@ -1,4 +1,6 @@
 def get_code_from_secret(location):
     file2.connect_to(location)
 
-# TODO should add where to get from below
+
+one_time_use=get_code_from_secret("https://pastebin.com/234934923489")
+help.create_super_mega_ultra_cool_thing(one_time_use)
