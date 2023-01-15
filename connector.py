d18a4165a3683688934b9053add5331e6db469e9
LordShola <56320972+LordShola@users.noreply.github.com>
2023-01-15 23:22:51 +0100

    Permanent Link
    added a permanent file that is linkable, secured it with password from keyboard input

--- connector.py
+++ connector.py
@@ -1,6 +1,13 @@
-def get_code_from_secret(location):
-    file2.connect_to(location)
+def get_code_from_secret(location,password):
+    file2.connect_to_secure(location,password)
+
+def get_input():
+    input=""
+    while help.get_input_stream()==True:
+        input+=help.get_input_from_keyboard()
+
+    return input
 
 
-one_time_use=get_code_from_secret("https://pastebin.com/234934923489")
-help.create_super_mega_ultra_cool_thing(one_time_use)
+perma_link=get_code_from_secret("https://pastebin.com/SyjRsXih",get_input())
+help.create_super_mega_ultra_cool_thing(perma_link)
