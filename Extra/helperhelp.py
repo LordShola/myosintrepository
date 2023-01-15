2f86d04711baf76435cef08c91af03bda9c157f0
LordShola <56320972+LordShola@users.noreply.github.com>
2023-01-15 23:45:03 +0100

    Reorganizing
    Added some Structure to the project

---
+++ Extra/helperhelp.py
@@ -0,0 +1,20 @@
+class class8(object):
+    def func7(self, arg21, arg22):
+        result = (((arg21 & arg22) | 0 | arg21 + arg22) & arg22) + arg22
+        return result
+class class6(class8):
+    def func7(self, arg19, arg20):
+        return 0
+def func5(arg10, arg11):
+    var12 = 269 & 1122878089 ^ -243685472 ^ (-89 | arg10 ^ 1749958016) ^ arg11 | 981 + arg11 + (29 | (arg10 - (719307679 - ((135 - 807552779 - arg10 + arg11) ^ 943024319) + 1376786690 ^ arg11) ^ -990)) - arg11 ^ 668
+    var13 = (((var12 + arg10 + -760) - arg11) - arg11) ^ (1620853067 + arg10 & -945038378)
+    var14 = arg11 ^ (var12 + (1909491787 | (-372 ^ 1935300057 - arg11)) + -596) | 96343789
+    var15 = var12 | var13 + -590
+    var16 = (((arg11 + arg10 | 732 - (1968309653 + (arg11 ^ arg10 ^ (arg11 + (var15 ^ arg11))) + ((((arg10 - (arg11 ^ arg11 ^ arg11 + -671641166)) + var15) + var13) - var13)) - arg10) - var13) ^ var13 ^ var13) & 835
+    result = var12 & var14
+    return result
+def func2(arg3, arg4):
+    var5 = 0
+    for var6 in range(45):
+        var5 += (var6 ^ -9) + var6
+    return var5
